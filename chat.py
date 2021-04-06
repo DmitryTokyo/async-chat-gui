31a7ee44d1953d514ce2f2c0b7f669b727c888de
andimeon <andimeon@gmail.com>
2021-04-06 16:09:30 +0900

    Saved messages history to file

--- chat.py
+++ chat.py
@@ -1,4 +1,6 @@
 import asyncio
+import aiofiles
+from datetime import datetime
 
 
 async def tcp_echo_client():
@@ -7,6 +9,10 @@ async def tcp_echo_client():
             'minechat.dvmn.org', 5000)
 
         data = await reader.readline()
-        print(data.decode(), end="")
+        message_time = datetime.now().strftime('[%d.%m.%y %H:%M]')
+        print(f'{message_time} {data.decode()}', end='')
+
+        async with aiofiles.open('tmp.txt', 'a') as file:
+            await file.write(f'{message_time} {data.decode()}')
 
 asyncio.run(tcp_echo_client())
