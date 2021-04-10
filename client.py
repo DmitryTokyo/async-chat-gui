fbc72fa6d925c1de28989c0306fa627669fc7f0d
andimeon <andimeon@gmail.com>
2021-04-10 17:14:22 +0900

    Refactor code, split by reusable functions.

--- client.py
+++ client.py
@@ -1,28 +1,87 @@
 import asyncio
+import logging
+import json
 from config import get_config
-from access import access_to_chat, get_user_hash
+
+logger = logging.getLogger('server')
+logging.basicConfig(level=logging.DEBUG)
 
 
-async def write_to_chat(host, port, user_hash):
+async def submit_message(host, port, user_hash):
     reader, writer = await asyncio.open_connection(host, port)
-    access = await access_to_chat(reader, writer, user_hash)
-    if access:
+    authorisation = await authorise(reader, writer, user_hash)
+    logger.info('For leaving chat type: Exit!')
+
+    if authorisation:
         while True:
             message = input('Your message: ')
-            send_message(writer, message)
+            if message == 'Exit!':
+                writer.close()
+                await writer.wait_closed()
+                break
+
+            writer.write(message.encode())
+            writer.write('\n\n'.encode())
 
 
-def send_message(writer, message):
-    writer.write(message.encode())
+async def authorise(reader, writer, user_hash):
+    response = await reader.readline()
+    logger.debug(response.decode())
+
+    writer.write(user_hash.encode())
     writer.write('\n\n'.encode())
+    
+    response = await reader.readline()
+    logger.debug(response.decode())
+
+    if not json.loads(response.decode()):
+        logger.error(f'User hash: {user_hash}')
+        logger.error('''Неизвестный токен. Проверьте его или
+                               зарегистрируйте заново.''')
+        return False
+
+    return True
+
+
+async def register(host, port):
+    print('''Для доступа к чату нужен user hash. Если у вас он есть,
+    укажите его при запуске скрипта. Если нет, получите новый user hash.''')
+
+    response = input('Вам нужен новый user_hash (y/n)? ')
+    if response == 'n':
+        return None
+
+    reader, writer = await asyncio.open_connection(host, port)
+
+    response = await reader.readline()
+    logger.debug(response.decode())
+    writer.write('\n'.encode())
+    response = await reader.readline()
+    logger.debug(response.decode())
+
+    nicname = input()
+    writer.write(nicname.encode())
+    writer.write('\n'.encode())
+
+    response = await reader.readline()
+    logger.debug(response.decode())
+    user_info = json.loads(response.decode())
+
+    with open('config.ini', 'a') as file:
+        file.write(f'\nuser_hash={user_info["account_hash"]}')
+
+    writer.close()
+    await writer.wait_closed()
+    return user_info['account_hash']
 
 
 async def main(config):
-    if not config.user_hash:
-        user_hash = await get_user_hash(config.host, config.port_in)
-        
-    await write_to_chat(config.host, config.port_in, config.user_hash)
+    user_hash = config.user_hash
+    if not user_hash:
+        user_hash = await register(config.host, config.port_in)
     
+    await submit_message(config.host, config.port_in, user_hash)
+
 
 if __name__ == '__main__':
     config = get_config()
