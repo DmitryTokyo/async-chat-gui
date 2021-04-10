e891971b46a30ffed1da6bcf53560da669727def
andimeon <andimeon@gmail.com>
2021-04-10 16:36:00 +0900

    Updated chechinkg user hash

--- client.py
+++ client.py
@@ -1,11 +1,6 @@
 import asyncio
-import logging
-import json
 from config import get_config
-
-logger_sender = logging.getLogger('sender')
-logger_client = logging.getLogger('client')
-logging.basicConfig(level=logging.DEBUG)
+from access import access_to_chat, get_user_hash
 
 
 async def write_to_chat(host, port, user_hash):
@@ -17,27 +12,17 @@ async def write_to_chat(host, port, user_hash):
             send_message(writer, message)
 
 
-async def access_to_chat(reader, writer, user_hash):
-    response = await reader.readline()
-    logger_sender.debug(response.decode())
-    logger_client.debug(user_hash)
-    send_message(writer, user_hash)
-    response = await reader.readline()
-    logger_sender.debug(response.decode())
-    if not json.loads(response.decode()):
-        logger_sender.error('Неизвестный токен. Проверьте его или зарегистрируйте заново.')
-        return False
-    return True
-
-
 def send_message(writer, message):
     writer.write(message.encode())
     writer.write('\n\n'.encode())
 
 
 async def main(config):
+    if not config.user_hash:
+        user_hash = await get_user_hash(config.host, config.port_in)
+        
     await write_to_chat(config.host, config.port_in, config.user_hash)
-
+    
 
 if __name__ == '__main__':
     config = get_config()
