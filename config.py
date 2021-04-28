f0532ecdfc818b45669419f7d2241d13e35f271d
andimeon <andimeon@gmail.com>
2021-04-28 22:48:58 +0900

    Updated config setup

--- config.py
+++ config.py
@@ -1,25 +1,50 @@
 import configargparse
+import json
 
 
-def get_config():
-    parser = configargparse.ArgParser(default_config_files=['*.conf'])
+def get_server_config():
+    parent_parser = set_parent_arguments()
+    parser = configargparse.ArgParser(parents=[parent_parser])
 
-    parser.add_argument('--host', type=str, help='host name', default='minechat.dvmn.org')
-    parser.add_argument('--port_out', type=int, help='chat port out (server.py)', default=5000)
-    parser.add_argument('--path', type=str, help='chat file path', default='./chat.txt')
-    parser.add_argument('--server', action='store_true',
-                        help='update server config file')
-
-    parser.add_argument('--user_hash', type=str, help='user hash')
-    parser.add_argument('--nickname', type=str, help='nickname')
-    parser.add_argument('--port_in', type=int, help='chat port in (client.py)', default=5050)
-    parser.add_argument('--user_conf', action='store_true', help='save user config')
+    parser.add_argument('--host', type=str, help='host name', 
+                        default='minechat.dvmn.org')
+    parser.add_argument('--port_out', type=int, help='chat port out (server.py)', 
+                        default=5000)
+    parser.add_argument('--path', type=str, help='chat file path', 
+                        default='./chat.txt')
+    parser.add_argument('--upd_server_file', action='store_true',
+                        help='update server config file information')
     
     config, unknown = parser.parse_known_args()
 
-    if config.server:
+    if not config.host or not config.port_out:
+        print('Необходимо указать имя хоста (--host) и порт (--port_out)')
+        return None
+
+    if config.upd_server_file:
         update_server_config_file(config)
     return config
+
+
+def get_client_config():
+    parent_parser = set_parent_arguments()
+    parser = configargparse.ArgParser(parents=[parent_parser])
+    parser.add_argument('--user_hash', type=str, help='user hash')
+    parser.add_argument('--nickname', type=str, help='nickname')
+    parser.add_argument('--port_in', type=int, help='chat port in (client.py)', 
+                        default=5050)
+    parser.add_argument('--upd_user_file', action='store_true', 
+                        help='save user information to config file')
+    config, unknown = parser.parse_known_args()
+
+    return config
+
+
+def set_parent_arguments():
+    parser = configargparse.ArgParser(default_config_files=['*.conf'], add_help=False)
+    parser.add_argument('--host', type=str, help='host name', 
+                        default='minechat.dvmn.org')
+    return parser
 
 
 def update_server_config_file(config):
@@ -28,3 +53,11 @@ def update_server_config_file(config):
         file.write(f'port_out={config.port_out}\n')
         file.write(f'port_in={config.port_in}\n')
         file.write(f'path={config.path}\n')
+        
+
+def update_user_config(response):
+    user_info = json.loads(response.decode())
+
+    with open('user.conf', 'w') as file:
+        file.write(f'user_hash={user_info["account_hash"]}\n')
+        file.write(f'nickname={user_info["nickname"]}\n')
