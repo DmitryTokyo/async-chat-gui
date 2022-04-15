fdd1a7527aa7cd8c3b5747891aa8bcbeaf790bf3
Dmitry <andimeon@gmail.com>
2022-04-15 19:16:10 +0900

    Split authorize function

--- autorization.py
+++ autorization.py
@@ -8,17 +8,21 @@ logger = logging.getLogger(__file__)
 
 
 async def authorize(reader, writer, user_hash, upd_user_file):
+    user_hash_identification_response = await get_user_identification_response(reader, writer, user_hash)
+    logger.debug(user_hash_identification_response.decode())
+
+    if not json.loads(user_hash_identification_response.decode()):
+        raise HashError
+
+    if upd_user_file:
+        save_user_info(user_hash_identification_response)
+
+
+async def get_user_identification_response(reader, writer, user_hash):
     response = await reader.readline()
     logger.debug(response.decode())
 
     writer.write(user_hash.encode())
     writer.write('\n\n'.encode())
 
-    response = await reader.readline()
-    logger.debug(response.decode())
-
-    if not json.loads(response.decode()):
-        raise HashError
-
-    if upd_user_file:
-        save_user_info(response)
+    return await reader.readline()
