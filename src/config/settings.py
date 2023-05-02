072949969868c8e831d7ced25030413b0c6103b2
andimeon <andimeon@gmail.com>
2023-05-02 13:48:24 +0900

    Create settings file for default settings

---
+++ src/config/settings.py
@@ -0,0 +1,10 @@
+import os
+
+
+CHAT_HISTORY_SAVE_PATH = os.getenv('CHAT_HISTORY_SAVE_PATH', default='./src/chat_history/chat.txt')
+
+USER_CONFIGURATION_FILE_PATH = os.getenv('USER_CONFIGURATION_SAVE_PATH', default='./src/config/user.conf')
+
+SERVER_CONFIGURATION_FILE_PATH = os.getenv('SERVER_CONFIGURATION_SAVE_PATH', default='./src/config/server.conf')
+
+DEFAULT_HOST_NAME = os.getenv('DEFAULT_HOST_NAME', default='minechat.dvmn.org')
